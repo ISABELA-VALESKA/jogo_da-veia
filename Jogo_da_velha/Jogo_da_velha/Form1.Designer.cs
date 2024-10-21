8111025b1622a7dd705e5ddfaceafb151a17295b
ISABELA-VALESKA <12301787@aluno.cotemig.com.br>
2024-10-21 12:42:02 -0300

    Add files via upload

---
+++ Jogo_da_velha/Jogo_da_velha/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Jogo_da_velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblJogo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLinha = new System.Windows.Forms.TextBox();
+            this.lblJogador = new System.Windows.Forms.Label();
+            this.txtColuna = new System.Windows.Forms.TextBox();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.limpa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblJogo
+            // 
+            this.lblJogo.AutoSize = true;
+            this.lblJogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogo.Location = new System.Drawing.Point(269, 116);
+            this.lblJogo.Name = "lblJogo";
+            this.lblJogo.Size = new System.Drawing.Size(0, 24);
+            this.lblJogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(202, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jogador da vez: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite o número da linha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(202, 325);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Digite o número da coluna: ";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(322, 378);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 24);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(202, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Vencedor:";
+            // 
+            // txtLinha
+            // 
+            this.txtLinha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLinha.Location = new System.Drawing.Point(423, 284);
+            this.txtLinha.Name = "txtLinha";
+            this.txtLinha.Size = new System.Drawing.Size(100, 29);
+            this.txtLinha.TabIndex = 6;
+            // 
+            // lblJogador
+            // 
+            this.lblJogador.AutoSize = true;
+            this.lblJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogador.Location = new System.Drawing.Point(348, 235);
+            this.lblJogador.Name = "lblJogador";
+            this.lblJogador.Size = new System.Drawing.Size(0, 24);
+            this.lblJogador.TabIndex = 7;
+            // 
+            // txtColuna
+            // 
+            this.txtColuna.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtColuna.Location = new System.Drawing.Point(436, 325);
+            this.txtColuna.Name = "txtColuna";
+            this.txtColuna.Size = new System.Drawing.Size(100, 29);
+            this.txtColuna.TabIndex = 8;
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogar.Location = new System.Drawing.Point(402, 393);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(121, 45);
+            this.btnJogar.TabIndex = 9;
+            this.btnJogar.Text = "JOGAR";
+            this.btnJogar.UseVisualStyleBackColor = true;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // limpa
+            // 
+            this.limpa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limpa.Location = new System.Drawing.Point(529, 393);
+            this.limpa.Name = "limpa";
+            this.limpa.Size = new System.Drawing.Size(121, 45);
+            this.limpa.TabIndex = 10;
+            this.limpa.Text = "LIMPA";
+            this.limpa.UseVisualStyleBackColor = true;
+            this.limpa.Click += new System.EventHandler(this.limpa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.limpa);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.txtColuna);
+            this.Controls.Add(this.lblJogador);
+            this.Controls.Add(this.txtLinha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblJogo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblJogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLinha;
+        private System.Windows.Forms.Label lblJogador;
+        private System.Windows.Forms.TextBox txtColuna;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Button limpa;
+    }
+}
+
---
+++ Jogo_da_velha/Jogo_da_velha/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_velha
+{
+    public partial class Form1 : Form
+    {
+        int vezes = 0;
+        char[,] matriz = new char[3, 3];
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            lblJogo.Text = " ";
+
+            int coluna = int.Parse(txtColuna.Text) - 1;
+            int linha = int.Parse(txtLinha.Text) - 1;
+            char jogador;
+
+            if (coluna > 2 || linha > 2 || coluna < 0 || linha < 0)
+            {
+                MessageBox.Show("Por favor, digite um valor válido!", "AVISO!");
+
+            }
+            if (matriz[linha, coluna] == 'X' || matriz[linha, coluna] == 'O')
+            {
+                MessageBox.Show("A célula já está ocupada! Escolha outra.", "AVISO!");
+                return; 
+            }
+            if (vezes % 2 == 0)
+            {
+                jogador = 'X';
+
+            }
+            else
+            {
+                jogador = 'O';
+            }
+                    vezes++;
+
+                    lblJogador.Text = jogador.ToString();
+
+                    matriz[linha, coluna] = jogador;
+
+                    if (vezes > 5)
+                    {
+                        if (matriz[0, 0] == jogador && matriz[1, 1] == jogador && matriz[2, 2] == jogador)
+                        {
+                            lblResultado.Text = jogador.ToString();
+                        }
+                        else if (matriz[2, 0] == jogador && matriz[1, 1] == jogador && matriz[0, 2] == jogador)
+                        {
+                            lblResultado.Text = jogador.ToString();
+                        }
+                        else {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                if (matriz[i, 0] == jogador && matriz[i, 1] == jogador && matriz[i, 2] == jogador)
+                                {
+                                    lblResultado.Text = jogador.ToString();
+                                }
+                                else if (matriz[0, i] == jogador && matriz[1, i] == jogador && matriz[2, i] == jogador)
+                                {
+                                    lblResultado.Text = jogador.ToString();
+                                }
+                            }
+                        }
+                    }
+                
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (matriz[i,j] == 'X'|| matriz[i, j] == 'O')
+                    {
+                        lblJogo.Text += matriz[i, j] + " ";
+                    }
+                    else
+                    {
+                        lblJogo.Text +=  "   ";
+                    }
+                    
+                }
+
+                lblJogo.Text += "\n";
+
+
+            }
+
+
+            if (vezes % 2 == 0)
+            {
+                jogador = 'X';
+
+            }
+            else
+            {
+                jogador = 'O';
+            }
+
+            lblJogador.Text = jogador.ToString();
+
+
+
+        }
+
+        private void limpa_Click(object sender, EventArgs e)
+        {
+            txtColuna.Clear();
+            txtLinha.Clear();
+
+        }
+    }
+}
